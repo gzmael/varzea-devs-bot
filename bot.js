a37d123b0680cedc94d3023d6d87aeade136d2fe
Jezmael Basilio <jezmaelbasilio@gmail.com>
2020-07-17 00:36:01 -0300

    teste CI Buddy

--- bot.js
+++ bot.js
@@ -1,26 +1,26 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = { prefix: '!' };
+const config = { prefix: "!" };
 
-const WelcomeService = require('./Services/Welcome');
+const WelcomeService = require("./Services/Welcome");
 
-client.on('ready', () => {
-    client.user.setActivity(`Let's Code`);
-    console.log('O bot está on-line');
+client.on("ready", () => {
+  client.user.setActivity(`Let's Code`);
+  console.log("O bot está on-line.");
 });
 
-client.on('guildMemberAdd', async member => {
-    const welcomeService = new WelcomeService(member);
+client.on("guildMemberAdd", async (member) => {
+  const welcomeService = new WelcomeService(member);
 
-    await welcomeService.generateImage();
+  await welcomeService.generateImage();
 });
 
-client.on('message', msg => {
-	if(msg.content === "Olá!"){
-		msg.reply('Olá meu amigo');
-	}
+client.on("message", (msg) => {
+  if (msg.content === "Olá!") {
+    msg.reply("Olá meu amigo");
+  }
 });
 
 client.login(process.env.AUTH_TOKEN);
